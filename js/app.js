19e542678ef0dde45d4eca251fceb5bdf7319bc2
robeurve <rurrutia1996@gmail.com>
2021-10-06 21:21:10 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// const burger = document.querySelector("#burger-button");
+// const menu = document.querySelector("#menu-nav");
+// const links = document.querySelector("#menu-links");
+// const body = document.querySelector("body");
+// const title = document.querySelector("#title");
+
+// burger.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   showMenu();
+// });
+
+// const showMenu = () => {
+//   menu.classList.toggle("show");
+//   links.classList.toggle("only-desktop");
+//   body.classList.toggle("overflow-h");
+//   title.classList.toggle("d-none");
+// };
